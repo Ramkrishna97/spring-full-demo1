a12a701ab9d68fd39dd21e2e61a523369dc66852
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2025-12-24 16:07:03 +0530

    Files added

---
+++ src/main/java/com/example/springfulldemo1/SpringFullDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springfulldemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFullDemo1Application.class, args);
+    }
+
+}
